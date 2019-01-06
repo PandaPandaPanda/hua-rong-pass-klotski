8463545c884c02d525844fde2973cf5f9caeb0a6
PandaPandaPanda <34975856+PandaPandaPanda@users.noreply.github.com>
2019-01-05 22:13:04 -0500

    Update Main.java

--- Hua Rong Pass 2.3/src/hua_rong_pass/Main.java
+++ Hua Rong Pass 2.3/src/hua_rong_pass/Main.java
@@ -1,11 +1,16 @@
 package hua_rong_pass;
 
-/**
- * Add comment Add pictures Add tutorial
- * 
- * @author xuyuxiang
- *
- */
+/************************************
+ * Author Kevin(Yuxiang) Xu
+ * Date: Jan. 5, 2019
+ * Title Hua_Rong_Pass
+ * Description: A small Klotski game
+ ************************************/
+
+//======================================//
+//Kevin(Yuxiang) Xu All Rights Reserved //
+//======================================//
+
 public class Main {
 
 	public static void main(String[] args) {
